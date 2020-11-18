fe3f2987271eba7aacf6a7aa5665eb00b4609265
krylenger <krylenger@gmail.com>
2020-11-19 00:29:02 +0800

    creating the sound version of the project

--- app.js
+++ app.js
@@ -1,35 +1,64 @@
+const orelSong = new Audio('orel.mp3');
+const gopgopSong = new Audio('gopgop.mp3');
+const tanciSong = new Audio('tanci.mp3');
+const cadilacSong = new Audio('cadilac.mp3');
+const dimSong = new Audio('dim.mp3');
+
 const cards = [
     {
         key: 1,
-        text: 'HTTP'
+        text: 'orel',
+        musicCard: false
     },
     {
         key: 1,
-        text: 'Hyper Text Transfer Protocol'
+        text: '$',
+        musicCard: true,
+        song: orelSong
     },
     {
         key: 2,
-        text: 'HTML'
+        text: 'gopgop',
+        musicCard: false
     },
     {
         key: 2,
-        text: 'Hyper Text Markup Language'
+        text: '$',
+        musicCard: true,
+        song: gopgopSong
     },
     {
         key: 3,
-        text: 'CSS'
+        text: 'tanci',
+        musicCard: false
     },
     {
         key: 3,
-        text: 'Cascade Style Sheet'
+        text: '$',
+        musicCard: true,
+        song: tanciSong
     },
     {
         key: 4,
-        text: 'JS'
+        text: 'cadilac',
+        musicCard: false
     },
     {
         key: 4,
-        text: 'JavaScript'
+        text: '$',
+        musicCard: true,
+        song: cadilacSong
+    },
+    {
+        key: 5,
+        text: 'dim',
+        musicCard: false
+    },
+    {
+        key: 5,
+        text: '$',
+        musicCard: true,
+        song: dimSong
     }
 ]
 
@@ -37,17 +66,22 @@ const main = document.querySelector('.main');
 let mainGridContainer = document.querySelector('.main__grid-container');
 
 
-const createNewCard = (card) => {
+const createNewCard = (card, cardNumber) => {
     const newCard = document.createElement('div');
     const newCardFlipper = document.createElement('div');
     const newCardFlipperFront = document.createElement('div');
     const newCardFlipperBack = document.createElement('div');
-    newCard.classList.add('card');
+    if (card.musicCard) {
+        newCard.classList.add('card', `card--${cardNumber}`, 'musicCard')
+        newCardFlipperBack.classList.add(`card--music-img`);
+    } else {
+        newCard.classList.add('card', `card--${cardNumber}`);
+        newCardFlipperBack.classList.add(`card--${card.text}-img`);
+    }
     newCardFlipper.classList.add('card__flipper');
     newCardFlipper.setAttribute('ontouchstart', "this.classList.toggle('card__flipper--flip');")
     newCardFlipperFront.classList.add('card__flipper-front', card.key);
     newCardFlipperBack.classList.add('card__flipper-back');
-    newCardFlipperBack.textContent = card.text;
     newCard.appendChild(newCardFlipper);
     newCardFlipper.appendChild(newCardFlipperFront);
     newCardFlipper.appendChild(newCardFlipperBack);
@@ -61,8 +95,8 @@ const shuffleArr = (arr) => {
 const loadCards = (cardsArr) => {
     const shuffledCards = shuffleArr(cardsArr);
     const fragment = document.createDocumentFragment();
-    shuffledCards.forEach(card => {
-        const newCard = createNewCard(card);
+    shuffledCards.forEach((card, index) => {
+        const newCard = createNewCard(card, index);
         fragment.appendChild(newCard);
     })
     mainGridContainer.appendChild(fragment);
@@ -79,7 +113,7 @@ const resetGame = () => {
 }
 
 const handlePlayingCards = (cardStatus) => {
-    let playingNow = document.querySelectorAll(".playingNow");
+    const playingNow = document.querySelectorAll(".playingNow");
     playingNow.forEach((card) => {
         card.classList.remove("playingNow");
         setTimeout(() => {
@@ -89,7 +123,7 @@ const handlePlayingCards = (cardStatus) => {
 }
 
 const handleGameOver = (guessedCards) => {
-    if (guessedCards === 8) {
+    if (guessedCards === 10) {
         setTimeout(() => {
             alert('won!');
             resetGame();
@@ -98,12 +132,22 @@ const handleGameOver = (guessedCards) => {
     }
 }
 
+
+
+const handleMusicCard = (target) => {
+    if (target.parentNode.parentNode.classList.contains('musicCard')) {
+        const targetKeyNumber = target.classList.value.match(/\d/)[0];
+        const targetObj = cards.find((card) => card.musicCard && card.key === Number.parseInt(targetKeyNumber, 10));
+        console.log(targetObj);
+        targetObj.song.play();
+    };
+}
+
 const flipTwoCards = () => {
     let clickCounter = 0;
     let guessedCards = 0;
     let playingCards = [];
     mainGridContainer.addEventListener('click', ({target}) => {
-        console.log(target);
         if (
           target.classList.contains("card__flipper-front") &&
           !target.parentNode.classList.contains("guessed") &&
@@ -111,9 +155,10 @@ const flipTwoCards = () => {
         ) {
           target.parentNode.classList.toggle("card__flipper--flip");
           target.classList.add("playingNow");
+          handleMusicCard(target);
+
           clickCounter += 1;
           playingCards.push(target.classList.value);
-          console.log(playingCards);
           if (clickCounter === 2 && playingCards[0] === playingCards[1]) {
             handlePlayingCards('guessed');
             clickCounter = 0;
